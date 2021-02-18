b28272c57f09ab798014edf8f78ce0e63cf8d6c5
Javier Elio <javiereliomedina@gmail.com>
2021-02-18 07:09:55 +0100

    Unzip files inside the function and remove ".zip"

--- R/dangeo_get_data.R
+++ R/dangeo_get_data.R
@@ -12,29 +12,37 @@
 #' # Set Username and Passwordm in .Renviron (usethis::edit_r_environ())
 #'   id <- Sys.getenv("kortforsyningen_id")
 #'   password <- Sys.getenv("kortforsyningen_pwd")
-#'   SDE_user <- paste(id, password, sep = ":" )
+#'   set_user <- paste(id, password, sep = ":" )
 #'
 #' # Get data
-#'   get_SDE_data(ftp_folder = "CORINE",
+#'   dangeo_get_data(ftp_folder = "CORINE",
 #'     file_name  = "DK_CORINE_SHP_UTM32-WGS84.zip",
 #'     out_folder = "data/CORINE")
 #'
 #' @export
 
-get_SDE_data <- function(ftp_folder = ftp_folder,
-                         file_name = file_name,
-                         out_folder = out_folder,
-                         userpwd = SDE_user){
+dangeo_get_data <- function(ftp_folder = ftp_folder,
+                            file_name = file_name,
+                            out_folder = out_folder,
+                            userpwd = set_user){
+
   ftp <- "ftp://ftp.kortforsyningen.dk"
   ftp_url <- paste(ftp, ftp_folder, "", sep = "/")
   dir.create(out_folder)
+
+  # Download
   dl_h <- curl::new_handle()
-  curl::handle_setopt(dl_h, userpwd = userpwd, ftp_use_epsv = TRUE)
+  curl::handle_setopt(dl_h, userpwd = my_pwd, ftp_use_epsv = TRUE)
   curl::curl_fetch_disk(url = paste0(ftp_url, file_name),
                         path = paste(out_folder, file_name, sep = "/"),
                         handle = dl_h)
 
+
+  # Unzip
+  unzip(zipfile = paste(out_folder, file_name, sep = "/"),
+        exdir   = sub(".zip", "", paste(out_folder, file_name, sep = "/")))
+
+  # Remove .zip
+  file.remove(paste(out_folder, file_name, sep = "/"))
+
 }
-
-
-
