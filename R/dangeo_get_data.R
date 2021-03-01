a4d3223b83385946847599a1cf16316203840884
Javier Elio <javiereliomedina@gmail.com>
2021-03-01 13:12:29 +0100

    Add overwrite option (default = FALSE: do not download files it they already exist in the local repository)

--- R/dangeo_get_data.R
+++ R/dangeo_get_data.R
@@ -3,6 +3,7 @@
 #' @param ftp_folder Name of the ftp folder
 #' @param zip_name   Name of the zip file we would like to download..
 #' @param out_folder Name of the local folder where the data will be saved (by default it is set to the results of rappdirs::user_cache_dir()).
+#' @param overwrite  Overwrite previous files (by default overwrite = FALSE)
 #' @param userpwd    Username and Password (format = USERNAME:PASSWORD).
 #'
 #' @return Data from [https://download.kortforsyningen.dk/content/geodataprodukter](https://download.kortforsyningen.dk/content/geodataprodukter).
@@ -20,11 +21,32 @@
 #'
 #' @export
 
+
 dangeo_get_data <- function(ftp_folder = NULL,
                             zip_name = NULL,
                             out_folder = rappdirs::user_cache_dir(),
-                            userpwd = set_user){
-  if(gsub(".zip", "", zip_name) %in% list.files(out_folder)) {
+                            userpwd = set_user,
+                            overwrite = FALSE){
+
+  if(overwrite == TRUE) {
+
+    ftp <- "ftp://ftp.kortforsyningen.dk"
+    ftp_url <- paste(ftp, ftp_folder, "", sep = "/")
+    fs::file_create(out_folder)
+
+    # Download
+    dl_h <- curl::new_handle(CONNECTTIMEOUT = 80)
+    curl::handle_setopt(dl_h, userpwd = set_user, ftp_use_epsv = TRUE)
+    curl::curl_fetch_disk(url = paste0(ftp_url, zip_name),
+                          path = paste(out_folder, zip_name, sep = "/"),
+                          handle = dl_h)
+
+    # Unzip file and remove it (.zip)
+    unzip(zipfile = paste(out_folder, zip_name, sep = "/"),
+          exdir   = gsub(".zip", "", paste(out_folder, zip_name, sep = "/")))
+    fs::file_delete(paste(out_folder, zip_name, sep = "/"))
+
+  } else if(gsub(".zip", "", zip_name) %in% list.files(out_folder)) {
 
     print("Warning: the files already exist on the local directory and thay have not been downloaded")
 
